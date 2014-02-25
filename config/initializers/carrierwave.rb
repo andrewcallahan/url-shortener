008202ce0f98b17c239f03ae9935119b98980aea
Andrew Callahan <andrew.callahan@gmail.com>
2014-02-25 01:31:05 -0500

    attempt to fix heroku screenshot issue

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,4 +1,7 @@
 CarrierWave.configure do |config|
+  config.root = Rails.root.join('tmp')
+  config.cache_dir = 'carrierwave'
+
   config.fog_credentials = {
     :provider               => 'AWS',
     :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
